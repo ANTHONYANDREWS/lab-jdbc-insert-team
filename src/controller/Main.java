afa507f5cc03f1512cba4d2536725cd71d6d057a
ANTHONYANDREWS <30433910+ANTHONYANDREWS@users.noreply.github.com>
2020-03-22 20:27:08 +0530

    My work

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,20 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import dao.CityDAO;
+
+
+public class Main 
+
+{
+	public static void main(String[] args) 
+	{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		CityDAO cd=new CityDAO();
+		
+	}
+	
+}
 
--- src/dao/CityDAO.java
+++ src/dao/CityDAO.java
@@ -1,1 +1,23 @@
+package dao;
+
+import java.sql.Statement;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.City;
+import utility.ConnectionManager;
+
+public class CityDAO
+{
+	public City getCityByName(String name) throws Exception
+	{
+		Connection con = ConnectionManager.getConnection();
+		return null;
+		
+		
+	
+}
+}
 
